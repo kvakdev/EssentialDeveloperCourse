d47e3c012277a3e86cbabf9d86119488a3844b70
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-04-15 11:20:47 +0300

    initial commit

---
+++ EssentialDevelopper/EssentialDevelopper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EssentialDevelopper
+//
+//  Created by Andre Kvashuk on 4/15/19.
+//  Copyright © 2019 Andre Kvashuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
