24225668f22d252fc9317a0daccc820a3549cd42
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-04-16 15:42:20 +0300

    move HTTPClient to its own file

---
+++ EssentialDevelopper/EssentialDevelopper/FeedFeature/HTTPClient.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPClient.swift
+//  EssentialDevelopper
+//
+//  Created by Andre Kvashuk on 4/16/19.
+//  Copyright © 2019 Andre Kvashuk. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(HTTPURLResponse, Data)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> ())
+}
--- EssentialDevelopper/EssentialDevelopper/FeedFeature/RemoteFeedLoader.swift
+++ EssentialDevelopper/EssentialDevelopper/FeedFeature/RemoteFeedLoader.swift
@@ -8,15 +8,6 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(HTTPURLResponse, Data)
-    case failure(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> ())
-}
-
 public final class RemoteFeedLoader {
     
     private let url: URL
