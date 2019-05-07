9c27f2c1b393fd5492387764eb1b44ecb414e898
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-05-07 16:10:58 +0300

    guarantee retrieve empty cache has no side effects

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
@@ -34,4 +34,20 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "waiting for retrieve to complete")
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult){
+                case (.empty, .empty):
+                    break
+                default:
+                    XCTFail("expected empty got \(firstResult) and \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
 }
