d46fbb1a0d9da2247d2b09757776d2287fed8926
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-05-08 15:13:41 +0300

    uncomment test_operations_completeSerially and make timeout 10 sec as it is a probable issue solution

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
@@ -112,32 +112,32 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .empty)
     }
     
-//    func test_operations_completeSerially() {
-//        let sut = makeSUT()
-//        var operations = [XCTestExpectation]()
-//        
-//        let op1 = expectation(description: "Operation 1")
-//        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
-//            operations.append(op1)
-//            op1.fulfill()
-//        }
-//        
-//        let op2 = expectation(description: "Operation 2")
-//        sut.deleteCache { _ in
-//            operations.append(op2)
-//            op2.fulfill()
-//        }
-//        
-//        let op3 = expectation(description: "Operation 3")
-//        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
-//            operations.append(op3)
-//            op3.fulfill()
-//        }
-//        
-//        wait(for: [op1, op2, op3], timeout: 5.0)
-//        
-//        XCTAssertEqual(operations, [op1, op2, op3])
-//    }
+    func test_operations_completeSerially() {
+        let sut = makeSUT()
+        var operations = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            operations.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCache { _ in
+            operations.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            operations.append(op3)
+            op3.fulfill()
+        }
+        
+        wait(for: [op1, op2, op3], timeout: 10.0)
+        
+        XCTAssertEqual(operations, [op1, op2, op3])
+    }
 }
 
 //MARK: - Helpers
