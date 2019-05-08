4bb2d82ed7074f2b7588658d9ba215bcb02d1777
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-05-08 10:01:11 +0300

    guarantee retrieve delivers error on invalid data

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
@@ -21,12 +21,16 @@ class CodableFeedStore {
     }
     
     func retrieve(completion: @escaping (FeedRetrieveResult) -> Swift.Void) {
-        
         guard let data = try? Data(contentsOf: storeUrl) else {
             return completion(.empty)
         }
-        let cache = try! JSONDecoder().decode(FeedCache.self, from: data)
-        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+        
+        do {
+            let cache = try JSONDecoder().decode(FeedCache.self, from: data)
+            completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCallback) {
@@ -44,7 +48,7 @@ class CodableFeedStore {
 }
 
 class CodableFeedStoreTests: XCTestCase {
-    private let storeUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed-image.store")
+    private let testStoreUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed-image.store")
     
     override func setUp() {
         super.setUp()
@@ -59,11 +63,11 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func cleanUpCache() {
-        try? FileManager.default.removeItem(at: storeUrl)
+        try? FileManager.default.removeItem(at: testStoreUrl)
     }
     
     func makeSUT() -> CodableFeedStore {
-        return CodableFeedStore(storeUrl: storeUrl)
+        return CodableFeedStore(storeUrl: testStoreUrl)
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -105,6 +109,7 @@ class CodableFeedStoreTests: XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         
+        
         sut.insert(feed, timestamp: timestamp) { error in
             XCTAssertNil(error)
             
@@ -134,6 +139,14 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, result: expectedResult)
     }
     
+    func test_retrieve_deliversErrorOnCurruptedData() {
+        let sut = makeSUT()
+        
+        insertCurruptedData(url: testStoreUrl)
+        
+        expect(sut, result: .failure(anyNSError()))
+    }
+    
     @discardableResult
     func insert(_ sut: CodableFeedStore, feed: [LocalFeedImage], timestamp: Date) -> Error? {
         let exp = expectation(description: "waitin for insert to complete")
@@ -149,20 +162,24 @@ class CodableFeedStoreTests: XCTestCase {
         return insertionError
     }
     
-    func expect(_ sut: CodableFeedStore, result: FeedRetrieveResult) {
+    func insertCurruptedData(url: URL) {
+        let data = "corrupted data".data(using: .utf8)
+        try! data?.write(to: url, options: .atomic)
+    }
+    
+    func expect(_ sut: CodableFeedStore, result: FeedRetrieveResult, file: StaticString = #file, line: UInt = #line) {
         sut.retrieve(completion: { retrievedResult in
             switch (retrievedResult, result) {
                 
             case (.found(let retrievedFeed, let retrievedTimestamp), .found(let expectedFeed, let expectedTimestamp)):
-                XCTAssertEqual(retrievedFeed, expectedFeed)
-                XCTAssertEqual(retrievedTimestamp, expectedTimestamp)
+                XCTAssertEqual(retrievedFeed, expectedFeed, file: file, line: line)
+                XCTAssertEqual(retrievedTimestamp, expectedTimestamp, file: file, line: line)
                 
-            case (.empty, .empty): break
+            case (.empty, .empty),
+                 (.failure, .failure): break
                 
-            case (.failure(let retrievedError), .failure(let expectedError)):
-                XCTAssertEqual(retrievedError as NSError, expectedError as NSError)
             default:
-                XCTFail("expected \(result) got \(retrievedResult) instead")
+                XCTFail("expected \(result) got \(retrievedResult) instead file: \(file), line: \(line)")
             }
         })
     }
