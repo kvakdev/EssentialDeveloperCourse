e9bba65ea334f4e8c0d23912339da7dbcadb705f
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-06-06 11:45:16 +0300

    return empty result on empty cache

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreDataFeedStoreTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreDataFeedStoreTests.swift
@@ -21,6 +21,12 @@ class CoreDataFeedStore {
         container = try NSPersistentContainer.load(url: url, name: "FeedStore", bundle: bundle)
         context = container.newBackgroundContext()
     }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCallback) {}
+    
+    func retrieve(completion: @escaping (FeedRetrieveResult) -> Swift.Void) {
+        completion(.empty)
+    }
 }
 
 class CoreDataFeedStoreTests: XCTestCase {
@@ -29,6 +35,22 @@ class CoreDataFeedStoreTests: XCTestCase {
         let sut = makeSUT()
         
         XCTAssertNotNil(sut)
+    }
+    
+    func test_retrieve_onEmptyCacheReturnsEmptyArray() {
+        let sut = makeSUT()
+        let exp = expectation(description: "waiting for retrieve")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("unexpected non empty result")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
     
     func makeSUT() -> CoreDataFeedStore {
