2ae1ee1139d43a4e2ef47c139f59fdfb9af882e0
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-06-06 11:30:55 +0300

    move extension code to the bottom of the file

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreDataFeedStoreTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreDataFeedStoreTests.swift
@@ -10,30 +10,6 @@ import XCTest
 import EssentialDeveloperFramework
 import CoreData
 
-extension NSManagedObjectModel {
-    static func with(name: String, bundle: Bundle) throws -> NSManagedObjectModel {
-        guard let path = bundle.path(forResource: name, ofType: "momd") else {
-            throw anyNSError()
-        }
-        
-        let url = URL(fileURLWithPath: path)
-        
-        return NSManagedObjectModel(contentsOf: url)!
-    }
-}
-
-extension NSPersistentContainer {
-    internal static func load(url: URL, name: String, bundle: Bundle) throws -> NSPersistentContainer {
-        let model = try NSManagedObjectModel.with(name: name, bundle: bundle)
-        let description = NSPersistentStoreDescription(url: url)
-        
-        let container = NSPersistentContainer(name: name, managedObjectModel: model)
-        container.persistentStoreDescriptions = [description]
-        
-        return container
-    }
-}
-
 class CoreDataFeedStore {
     let url: URL
     let container: NSPersistentContainer
@@ -63,5 +39,31 @@ class CoreDataFeedStoreTests: XCTestCase {
         trackMemoryLeaks(sut)
         
         return sut
+    }
+}
+
+
+
+extension NSManagedObjectModel {
+    static func with(name: String, bundle: Bundle) throws -> NSManagedObjectModel {
+        guard let path = bundle.path(forResource: name, ofType: "momd") else {
+            throw anyNSError()
+        }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        return NSManagedObjectModel(contentsOf: url)!
+    }
+}
+
+extension NSPersistentContainer {
+    internal static func load(url: URL, name: String, bundle: Bundle) throws -> NSPersistentContainer {
+        let model = try NSManagedObjectModel.with(name: name, bundle: bundle)
+        let description = NSPersistentStoreDescription(url: url)
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [description]
+        
+        return container
     }
 }
