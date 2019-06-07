d5f0d4e96c0e4df942f85d4322077367e4a89dfe
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-06-07 10:14:34 +0300

    rearrange CoreDataStore related files into FeedCache folder

---
+++ EssentialDevelopper/EssentialDeveloperFramework/FeedCache/CoreData/CoreDataHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataHelpers.swift
+//  EssentialDeveloperFramework
+//
+//  Created by Andre Kvashuk on 6/7/19.
+//  Copyright © 2019 Andre Kvashuk. All rights reserved.
+//
+
+import Foundation
---
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,9 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialDeveloperFrameworkTests
+//
+//  Created by Andre Kvashuk on 6/6/19.
+//  Copyright © 2019 Andre Kvashuk. All rights reserved.
+//
+
+import Foundation
