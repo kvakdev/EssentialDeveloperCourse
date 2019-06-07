08a1a916e5a4019b03a664b02291b0b786ec0405
Andre Kvashuk <andremtcp2015a@gmail.com>
2019-06-07 10:26:31 +0300

    add ManagedFeedImage and modify project file

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreData/ManagedFeedImage.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/CacheFeedUseCaseTests/CoreData/ManagedFeedImage.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2019 Andre Kvashuk. All rights reserved.
 //
 
-import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+    @NSManaged internal var id: UUID
+    @NSManaged internal var imageDescription: String?
+    @NSManaged internal var location: String?
+    @NSManaged internal var url: URL
+}
+
+
+extension ManagedFeedImage {
+    func toManaged(from images: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        
+        return NSOrderedSet(array: images.compactMap { image in
+            let managed = ManagedFeedImage(context: context)
+         
+            managed.id = image.id
+            managed.imageDescription = image.description
+            managed.location = image.location
+            managed.url = image.url
+            
+            return managed
+        })
+    }
+    
+}
