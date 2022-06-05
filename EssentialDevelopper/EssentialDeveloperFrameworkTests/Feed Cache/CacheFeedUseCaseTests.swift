3ca3ce6b5414c082540b917717b38f9142c14b66
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-05 10:05:17 +0300

    Delete cache on saving feed images

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -7,22 +7,47 @@
 //
 
 import XCTest
+import EssentialDeveloperFramework
 
 class FeedStore {
     var deletionCallCount = 0
+    
+    func deleteCache() {
+        deletionCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
+    let store: FeedStore
+    
     init(_ store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ feedImages: [FeedImage]) {
+        store.deleteCache()
     }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
+    
     func test_deletionIsNotInvoked_uponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store)
         
         XCTAssertEqual(store.deletionCallCount, 0)
+    }
+    
+    func test_deletionIsCalled_uponInsertion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store)
+        let feedItems = [uniqueFeedImage(), uniqueFeedImage()]
+        sut.save(feedItems)
+        
+        XCTAssertEqual(store.deletionCallCount, 1)
+    }
+    
+    private func uniqueFeedImage() -> FeedImage {
+        return FeedImage(id: UUID(), imageUrl: anyURL())
     }
 }
