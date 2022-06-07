ec0585f99afb21cd8f2eab2f53ce1f98bcc44404
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-07 16:30:19 +0300

    Move `CodableFeedStore` and `CoreDataFeedStore` to infrastructure folder

--- EssentialDevelopper/EssentialDeveloperFramework/Feed Cache/Infrastructure/CodableFeedStore.swift
+++ EssentialDevelopper/EssentialDeveloperFramework/Feed Cache/Infrastructure/CodableFeedStore.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public class CodableFeedStore: FeedStore {
+public final class CodableFeedStore: FeedStore {
     
     private struct FeedContainer: Codable {
         let feed: [CodableFeedImage]
