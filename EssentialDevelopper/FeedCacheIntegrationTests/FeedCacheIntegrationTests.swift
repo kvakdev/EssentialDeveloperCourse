35cf8be9512c44b377d0fec42731f283239be1a3
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-07 17:20:09 +0300

    Add `FeedCacheIntergrationTests` target

---
+++ EssentialDevelopper/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCacheIntegrationTests.swift
+//  FeedCacheIntegrationTests
+//
+//  Created by Andre Kvashuk on 6/7/22.
+//  Copyright © 2022 Andre Kvashuk. All rights reserved.
+//
+
+import XCTest
+
+class FeedCacheIntegrationTests: XCTestCase {
+
+}
