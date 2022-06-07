547d4699621a249d6e32d44fc48e5c164b75851e
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-07 17:35:21 +0300

    Ensure load from empty cache return empty feed

--- EssentialDevelopper/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
+++ EssentialDevelopper/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -7,7 +7,43 @@
 //
 
 import XCTest
+import EssentialDeveloperFramework
 
 class FeedCacheIntegrationTests: XCTestCase {
-
+    
+    func test_cache_hasNoSideEffectsReadingFromAnEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "wait for load to complete")
+        
+        sut.load { result in
+            switch result {
+            case .success(let feed):
+                XCTAssertEqual(feed, [])
+            case .failure:
+                XCTFail("Load failed, expected empty feed")
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeURL = testSpecificaStoreURL()
+        let bundle = Bundle(for: CoreDataFeedStore.self)
+        let feedStore = try! CoreDataFeedStore(bundle: bundle, storeURL: storeURL)
+        let sut = LocalFeedLoader(feedStore, timestamp: { Date() })
+         
+        trackMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
+    }
+    
+    func testSpecificaStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    func cachesDirectory() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
