9985dda07e0dc2dd7c406254859bc1f5dc102f4c
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-10 14:49:15 +0300

    Setup EssentialFeed_iOS target with FeedViewControllerTests and gather coverage

---
+++ EssentialDevelopper/EssentialFeed_iOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeed_iOSTests
+//
+//  Created by Andre Kvashuk on 6/10/22.
+//  Copyright © 2022 Andre Kvashuk. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedViewControllerTests: XCTestCase {
+
+}
