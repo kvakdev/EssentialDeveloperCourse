065c3c910ffa4d23ed3b49d3734756dae29ef80d
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-17 12:41:51 +0300

    Move FeedLoaderUIModel to separate file

--- EssentialDevelopper/EssentialDeveloperFramework/Feed Presentation/FeedPresenter.swift
+++ EssentialDevelopper/EssentialDeveloperFramework/Feed Presentation/FeedPresenter.swift
@@ -13,22 +13,6 @@ public struct FeedUIModel {
     public let feed: [FeedImage]
 }
 
-public struct FeedLoaderUIModel {
-    public let isLoading: Bool
-    public let errorMessage: String?
-    
-    static var loading: FeedLoaderUIModel {
-        FeedLoaderUIModel(isLoading: true, errorMessage: nil)
-    }
-    
-    static var noError: FeedLoaderUIModel {
-        FeedLoaderUIModel(isLoading: false, errorMessage: nil)
-    }
-    
-    static func loadingError(_ message: String) -> FeedLoaderUIModel {
-        FeedLoaderUIModel(isLoading: false, errorMessage: message)
-    }
-}
 
 
 public protocol LoaderView {
