4662b3b2befbe00ec3ce0ad53f7399c827eb853e
Andre Kvashuk <andremtcp2015a@gmail.com>
2022-06-17 12:14:07 +0300

    Ensure display feed Image and hide loader and error messages are sent when loading successfully completes

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/FeedPresentation/FeedPresenterTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/FeedPresentation/FeedPresenterTests.swift
@@ -7,6 +7,11 @@
 //
 
 import XCTest
+import EssentialFeed
+
+struct FeedUIModel {
+    let feed: [FeedImage]
+}
 
 struct FeedLoaderUIModel {
     let isLoading: Bool
@@ -15,22 +20,38 @@ struct FeedLoaderUIModel {
     static var loading: FeedLoaderUIModel {
         FeedLoaderUIModel(isLoading: true, errorMessage: nil)
     }
+    
+    static var noError: FeedLoaderUIModel {
+        FeedLoaderUIModel(isLoading: false, errorMessage: nil)
+    }
 }
 
 protocol LoaderView {
     func display(uiModel: FeedLoaderUIModel)
 }
 
+protocol FeedView {
+    func display(model: FeedUIModel)
+}
+
 class FeedPresenter {
+    let view: FeedView
     let loaderView: LoaderView
     
-    init(loaderView: LoaderView) {
+    init(view: FeedView, loaderView: LoaderView) {
+        self.view = view
         self.loaderView = loaderView
     }
     
     func didStartLoadingFeed() {
         self.loaderView.display(uiModel: .loading)
     }
+    
+    func didCompleteLoading(with feed: [FeedImage]) {
+        self.view.display(model: FeedUIModel(feed: feed))
+        self.loaderView.display(uiModel: .noError)
+    }
+
 }
 
 class FeedPresenterTests: XCTestCase {
@@ -49,24 +70,39 @@ class FeedPresenterTests: XCTestCase {
             .display(error: nil)], "Expected loading and no error when loading starts.")
     }
     
-    private class ViewSpy: LoaderView {
+    func test_completeLoading_displaysFeedAndNoError() {
+        let (sut, view) = makeSUT()
+        let feed = [uniqueFeedImage()]
+        sut.didCompleteLoading(with: feed)
+        
+        XCTAssertEqual(view.messages, [
+            .display(feed: feed),
+            .display(isLoading: false),
+            .display(error: nil)])
+    }
+    
+    private class ViewSpy: LoaderView, FeedView {
+        
         enum Messages: Equatable {
             case display(isLoading: Bool)
             case display(error: String?)
+            case display(feed: [FeedImage])
         }
+        var messages: [Messages] = []
         
         func display(uiModel: FeedLoaderUIModel) {
             messages.append(.display(isLoading: uiModel.isLoading))
             messages.append(.display(error: uiModel.errorMessage))
         }
         
-        
-        var messages: [Messages] = []
+        func display(model: FeedUIModel) {
+            messages.append(.display(feed: model.feed))
+        }
     }
     
     private func makeSUT() -> (FeedPresenter, ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(loaderView: view)
+        let sut = FeedPresenter(view: view, loaderView: view)
         
         trackMemoryLeaks(sut)
         trackMemoryLeaks(view)
