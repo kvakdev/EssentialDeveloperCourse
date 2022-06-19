3b5a39e4c6d748d9015e0ddc1a09dfd3b5d1aa19
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-19 13:46:39 +0300

    Ensure image loaded over the network from static image url

--- EssentialDevelopper/FeedApiEndToEndTests/FeedApiEndToEndTests.swift
+++ EssentialDevelopper/FeedApiEndToEndTests/FeedApiEndToEndTests.swift
@@ -27,6 +27,40 @@ class FeedApiEndToEndTests: XCTestCase {
         }
     }
     
+    func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+             switch getFeedImageDataResult() {
+             case let .success(data)?:
+                 XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+
+             case let .failure(error)?:
+                 XCTFail("Expected successful image data result, got \(error) instead")
+
+             default:
+                 XCTFail("Expected successful image data result, got no result instead")
+             }
+         }
+
+    
+    private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> RemoteFeedImageLoader.Result? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteFeedImageLoader(client: client)
+        trackMemoryLeaks(client, file: file, line: line)
+        trackMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: RemoteFeedImageLoader.Result?
+        
+        _ = loader.loadImage(with: testServerURL) { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
+    
     func loadFeed(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result {
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let feedURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
