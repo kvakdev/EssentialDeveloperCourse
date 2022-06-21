65236baf96ac15561f5776a6f912e21d057d366e
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-21 17:15:58 +0300

    Ensure validation succeeds on EmptyCache

--- EssentialDevelopper/EssentialDeveloperFrameworkTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialDevelopper/EssentialDeveloperFrameworkTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -69,6 +69,46 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.savedMessages, [.retrieve])
     }
     
+    func test_validate_succeedsOnRetreivalErrorAndSuccessfulDeletion() {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store, timestamp: { Date() })
+        
+        expect(sut, toCompleteWith: .success(())) {
+            store.completeRetrieveWith(anyNSError())
+            store.successfulyCompleteDeletion()
+        }
+    }
+    
+    func test_validateCache_succeedsOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrieveWith([], timestamp: Date())
+        })
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toCompleteWith result: LocalFeedLoader.ValidationResult, when action: VoidClosure, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "wait for validation to complete")
+        
+        sut.validateCache { receivedResult in
+            switch (receivedResult, result) {
+            case (.success(()), .success(())):
+                break
+                
+            case (.failure(let receivedError), .failure(let expectedError)):
+                XCTAssertEqual((receivedError as NSError), (expectedError as NSError), file: file, line: line)
+                
+            default:
+                XCTFail("Expected to get \(result), but got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     private func makeSUT(timestamp: @escaping () -> Date = Date.init) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
         let sut = LocalFeedLoader(store, timestamp: timestamp)
