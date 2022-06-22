535aa36901e5dfa79a897635a165b3a6ec3daae0
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-22 18:54:12 +0300

    Add EssentialAppTest target

---
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedLoaderWithFallbackCompositTests.swift
+//  EssentialAppTests
+//
+//  Created by Andre Kvashuk on 6/22/22.
+//
+
+import XCTest
+
+class FeedLoaderWithFallbackCompositTests: XCTestCase {
+    func test_something() {
+        
+    }
+}
