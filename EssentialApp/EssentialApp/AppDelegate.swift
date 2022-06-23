caf3f2b6f74f880c4f3c4d87108378bc532d1e6a
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-23 15:39:31 +0300

    Add combined FeedLoader and combined FeedImageLoader to FeedViewController composition

--- EssentialApp/EssentialApp/AppDelegate.swift
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import EssentialFeed
 import EssentialFeed_iOS
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,15 +17,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let storeURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("Feed-store.sqlite")
+        let feedStore = try! CoreDataFeedStore(storeURL: storeURL)
         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         let session = URLSession(configuration: .ephemeral)
         let client = URLSessionHTTPClient(session: session)
-        let feedImageLoader = RemoteFeedImageLoader(client: client)
-        let feeeedLoader = RemoteFeedLoader(url: url, client: client)
+        
+        let remoteImageLoader = RemoteFeedImageLoader(client: client)
+        let localImageLoader = LocalFeedImageLoader(store: feedStore)
+        
+        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
+        let localFeedLoader = LocalFeedLoader(feedStore) { Date() }
+        
+        let combinedFeedLoader = FeedLoaderWithFallbackComposit(
+            primary: remoteFeedLoader,
+            fallback: localFeedLoader
+        )
+        let imageLoader = ImageLoaderWithFallbackComposit(
+            primaryLoader: remoteImageLoader,
+            fallbackLoader: localImageLoader
+        )
         
         let feedViewController = FeedUIComposer.makeFeedViewController(
-            loader: feeeedLoader,
-            imageLoader: feedImageLoader
+            loader: combinedFeedLoader,
+            imageLoader: imageLoader
         )
         
         window = UIWindow(frame: UIScreen.main.bounds)
