242608cd2a275f34d8ccf8da0c900f931fd8fe5e
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-26 20:18:15 +0300

    Display non empty feed

--- EssentialFeedStudy/EssentialFeed_iOSTests/FeedSnapshotTests.swift
+++ EssentialFeedStudy/EssentialFeed_iOSTests/FeedSnapshotTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import EssentialFeed_iOS
+@testable import EssentialFeed
 
 class FeedSnapshotTests: XCTestCase {
     func test_sut_displaysEmptyFeed() {
@@ -16,6 +17,14 @@ class FeedSnapshotTests: XCTestCase {
         
         let snapshot = sut.takeSnapshot()
         record(snapshot: snapshot, named: "EMPTY_FEED")
+    }
+    
+    func test_sut_displaysFeed() {
+        let sut = makeSUT()
+        sut.display(model: nonEmptyFeed())
+        
+        let snapShot = sut.takeSnapshot()
+        record(snapshot: snapShot, named: "NON_EMPTY_FEED")
     }
     
     func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
@@ -54,6 +63,39 @@ class FeedSnapshotTests: XCTestCase {
     private func emptyFeed() -> [FeedImageCellController] {
         return []
     }
+    
+    private func nonEmptyFeed() ->  [FeedImageCellController] {
+        let delegateOne = FeedImageCellControllerDelegateStubDelegate(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mattis quam, id dapibus ipsum. Nam vitae orci massa. Cras vel magna ut purus placerat elementum quis sed ante. ", image: UIImage.with(.red))
+        let delegateTwo = FeedImageCellControllerDelegateStubDelegate(description: "Lorem orci massa. Cras vel magna ut purus placerat elementum quis sed ante. ", image: UIImage.with(.blue), location: "New York Times Square")
+        
+        let cellControllerOne = FeedImageCellController(delegate: delegateOne)
+        let cellControllerTwo = FeedImageCellController(delegate: delegateTwo)
+        delegateOne.cellController = cellControllerOne
+        delegateTwo.cellController = cellControllerTwo
+        
+        
+        return [cellControllerOne, cellControllerTwo]
+    }
+}
+
+class FeedImageCellControllerDelegateStubDelegate: FeedImageCellControllerDelegate {
+    weak var cellController: FeedImageCellController?
+    let stubViewModel: FeedImageViewModel<UIImage>
+    
+    init(description: String, image: UIImage, location: String? = nil) {
+        self.stubViewModel = FeedImageViewModel<UIImage>(description: description,
+                                               location: location,
+                                               isLocationHidden: true,
+                                               isLoading: false,
+                                               image: image,
+                                               isRetryVisible: false)
+    }
+    
+    func didRequestToLoadImage() {
+        cellController?.display(model: stubViewModel)
+    }
+    
+    func didCancelTask() {}
 }
 
 extension UIViewController {
@@ -63,5 +105,18 @@ extension UIViewController {
         return renderer.image { action in
             self.view.layer.render(in: action.cgContext)
         }
+    }
+}
+
+extension UIImage {
+    static func with(_ color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
     }
 }
