8f2cd65c4af395bac28e71de6e347f98bef20c37
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-27 11:32:59 +0300

    Move snapshot window to separate file

--- EssentialFeedStudy/EssentialFeed_iOSTests/FeedSnapshotTests.swift
+++ EssentialFeedStudy/EssentialFeed_iOSTests/FeedSnapshotTests.swift
@@ -201,32 +201,3 @@ struct SnapshotConfiguration {
              ]))
      }
  }
-
-private final class SnapshotWindow: UIWindow {
-    private var configuration: SnapshotConfiguration = .iPhone8(style: .light)
-    
-    convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
-        self.init(frame: CGRect(origin: .zero, size: configuration.size))
-        self.configuration = configuration
-        self.layoutMargins = configuration.layoutMargins
-        self.rootViewController = root
-        self.isHidden = false
-        root.view.layoutMargins = configuration.layoutMargins
-    }
-    
-    override var safeAreaInsets: UIEdgeInsets {
-        return configuration.safeAreaInsets
-    }
-    
-    override var traitCollection: UITraitCollection {
-        return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
-    }
-    
-    func snapshot() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: self.bounds)
-        
-        return renderer.image { action in
-            self.layer.render(in: action.cgContext)
-        }
-    }
-}
