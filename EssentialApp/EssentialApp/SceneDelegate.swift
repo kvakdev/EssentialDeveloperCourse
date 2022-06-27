2278a218d754822a68f0b6994d38ba57616552b0
Andrii Kvashuk <andremtcp2015a@gmail.com>
2022-06-27 18:41:14 +0300

    Make window key and visible

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -29,8 +29,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard (scene is UIWindowScene) else { return }
-
+        guard let scene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: scene)
+        
         setup()
     }
     
